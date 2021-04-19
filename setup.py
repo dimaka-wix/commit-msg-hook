0254abb9dc016bcf91336d0779ae93b833e9067d
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-19 16:12:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+# Load the README file.
+with open(file="README.md", mode="r") as readme_handle:
+    README = readme_handle.read()
 
 setup(
+    # The name that used along with `pip install`
     name="commit-msg-hook",
-    version="0.0.0",
-    install_requires=["commit-msg-hook>=0.0.0"],
+    author="Dima Karpukhin",
+    author_email="dimadk787@gmail.com",
+    version="0.1.0",
+
+    # The description that appears when someone searches for the library on https://pypi.org/search
+    description="Checks if commit message matches the chaos-hub commit rules",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DimaKarpukhin/commit-msg-hook.git",
+
+
+    # The packages I want "build."
+    packages=find_packages(),
 )
