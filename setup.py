74a4ad031036bdb80bbc9f6f7c1b337013713342
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-19 16:29:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-# Load the README file.
-with open(file="README.md", mode="r") as readme_handle:
-    README = readme_handle.read()
 
 setup(
     # The name that used along with `pip install`
@@ -13,8 +10,6 @@ setup(
 
     # The description that appears on https://pypi.org/search
     description="Checks if commit message matches the chaos-hub commit rules",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/DimaKarpukhin/commit-msg-hook.git",
 
     # The packages I want "build."
