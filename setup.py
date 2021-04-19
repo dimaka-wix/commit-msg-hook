a7af03631d0ca01e3a3110bcc857f2f903c400dd
DimaKarpukhin <dimadk787@gmaile.com>
2021-04-19 14:45:30 +0300

    Test

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name="commit-msg-hook",
     version="0.0.0",
-    install_requires=["commit-msg-hook>=0.1.0"],
+    install_requires=["commit-msg-hook>=0.0.0"],
 )
